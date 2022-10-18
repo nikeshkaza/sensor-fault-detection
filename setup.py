ce7b21902e3503ef9d1f0315139de1a4ccd68634
nikeshkaza <nikesh.kaza7@gmail.com>
2022-10-18 20:45:38 +0530

    first commit - ML project setup done

---
+++ setup.py
@@ -0,0 +1,12 @@
+import email
+from setuptools import find_packages,setup
+
+setup(
+
+
+    name="sensor",
+    version="0.01",
+    author="Nikesh Kaza",
+    email="nikesh.kaza7@gmail.com",
+    packages=find_packages(),
+)
