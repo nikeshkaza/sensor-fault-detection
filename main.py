012a8304730c1b1954758fbd3d2e35ea0e6ac890
nikeshkaza <nikesh.kaza7@gmail.com>
2022-10-31 23:56:23 +0530

    mongo db rewired

--- main.py
+++ main.py
@@ -4,4 +4,5 @@ from sensor.configuration.mongo_db_connection import MongoDBClient
 
 if __name__ == '__main__':
     mongodb_client = MongoDBClient()
+    print("CLIENT name:",mongodb_client.database_name)
     print("collection name:",mongodb_client.database.list_collection_names())
--- sensor/constant/database.py
+++ sensor/constant/database.py
@@ -1,1 +1,2 @@
-DATABASE_NAME = "ineuron"
+DATABASE_NAME = "Neuron"
+PASSWORD = "OWUCXgQaZkCnE4ht"
