10fb533a9a5c395f67a4ceedf98e892a4a583f7b
nikeshkaza <nikesh.kaza7@gmail.com>
2022-11-01 19:36:50 +0530

    data ingestion completed

---
+++ sensor/constant/env_variable.py
@@ -0,0 +1,6 @@
+from sensor.constant.database import PASSWORD
+
+MONGODB_URL_KEY = f"mongodb+srv://nikeshkaza:{PASSWORD}@sensordetection.e9sdt23.mongodb.net/?retryWrites=true&w=majority"
+#AWS_ACCESS_KEY_ID_ENV_KEY = "AWS_ACCESS_KEY_ID"
+#AWS_SECRET_ACCESS_KEY_ENV_KEY = "AWS_SECRET_ACCESS_KEY"
+REGION_NAME = "centralindia"
